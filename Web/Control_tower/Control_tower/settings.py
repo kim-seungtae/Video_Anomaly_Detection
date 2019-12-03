0ea232355a85cc3b0a576fd062bc6502511c4430
kim-seungtae <tae1303@gmail.com>
2019-12-04 04:06:02 +0900

    [update] webpage

--- Web/Control_tower/Control_tower/settings.py
+++ Web/Control_tower/Control_tower/settings.py
@@ -120,5 +120,11 @@ USE_TZ = False
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+) 
+
+
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
