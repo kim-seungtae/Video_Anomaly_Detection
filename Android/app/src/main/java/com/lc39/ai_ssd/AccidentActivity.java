8e5bcef0af2cfdac28c7936999b7ea84bbb36f5b
khj68 <rlagudwns68@gmail.com>
2019-12-10 22:53:13 +0900

    [add] android project

---
+++ Android/app/src/main/java/com/lc39/ai_ssd/AccidentActivity.java
@@ -0,0 +1,13 @@
+package com.lc39.ai_ssd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccidentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accident);
+    }
+}
